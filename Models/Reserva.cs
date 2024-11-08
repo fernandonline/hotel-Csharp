d15713e9cb50a02d284b1bf64c62bc15759c5581
fernando <132716575+fernandonline@users.noreply.github.com>
2024-11-07 21:39:05 -0300

    Add files via upload

---
+++ Models/Reserva.cs
@@ -0,0 +1,59 @@
+namespace HotelCsharp.Models
+{
+    public class Reserva
+    {
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public int DiasReservados { get; set; }
+
+        public Reserva() 
+        {
+            Hospedes = new List<Pessoa>();
+        }
+
+        public Reserva(int diasReservados)
+        {
+            DiasReservados = diasReservados;
+            Hospedes = new List<Pessoa>();
+        }
+
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            // Verifica se a capacidade é maior ou igual ao número de hóspedes sendo recebido
+            if (hospedes.Count <= Suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            }
+            else
+            {
+                // Retorna uma exception caso a capacidade seja menor que o número de hóspedes recebido
+                throw new Exception("Quantidade de hóspedes maior que a capacidade da suite");
+            }
+        }
+
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            // Retorna a quantidade de hóspedes (propriedade Hospedes)
+            return Hospedes.Count;
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            // Cálculo: DiasReservados X Suite.ValorDiaria
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+
+            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
+            if (DiasReservados >= 10)
+            {
+                valor = valor * 0.9M;  // Aplica 10% de desconto
+            }
+
+            return valor;
+        }
+    }
+}
